ae4efe61b55a9146a6070b108317e1c04860799f
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-19 14:05:33 +0100

    actions added to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,39 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all
   end
 
   def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item)
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to item_path(@item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :category, :location, :image_url)
+  end
+
+  def set_item
     @item = Item.find(params[:id])
   end
 end
