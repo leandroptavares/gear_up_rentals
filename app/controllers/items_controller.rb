33b079bdf535540547e8f607062bc92d6ef79881
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-19 12:59:22 +0100

    add show action and show view page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!
 
   def index
     @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 end
