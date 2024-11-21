90616efb2c4fa5438eb2253bef68b0405ea39b05
leandroptavares@gmail.com <leandroptavares@gmail.com>
2024-11-21 20:36:19 +0100

    fixed reviews association to items, fixed banner placement, added times rented for user's items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :review, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
